fc08d44b3a1da6951644cfa065eb770cd728900a
Ilia539 <ilia.uliev@yandex.ru>
2020-11-27 16:19:42 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -44,4 +44,18 @@ const checkbtn = document.querySelector('.checkbtn');
     checkbtn.addEventListener('click', () => {
       headerMenu.classList.toggle('open');
       body.classList.toggle('lock');
-    })
+    });
+// ............SCROLL
+    const anchors = document.querySelectorAll('a[href*="#"]')
+
+    for (let anchor of anchors) {
+      anchor.addEventListener("click", function (event) { 
+        event.preventDefault();
+        const blockID = anchor.getAttribute('href')
+        document.querySelector('' + blockID).scrollIntoView({
+          behavior: "smooth",
+          block: "start"
+        })
+       })
+    }
+ 
